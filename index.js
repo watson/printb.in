2769d4fcc882a76301d081142a0529f59e7980bf
Thomas Watson Steen <w@tson.dk>
2015-12-15 00:20:16 +0100

    List document name if available

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ var opbeat = require('opbeat').start()
 var fs = require('fs')
 var http = require('http')
 var handlebars = require('handlebars')
+var afterAll = require('after-all')
 var AWS = require('aws-sdk')
 
 var s3 = new AWS.S3()
 var tmpl = handlebars.compile(fs.readFileSync('index.handlebars').toString())
+var BUCKET = 'watson-printbin'
+var names = {}
 
 var server = http.createServer(function (req, res) {
   if (req.method !== 'GET' || req.url !== '/') {
@@ -16,7 +19,7 @@ var server = http.createServer(function (req, res) {
     return
   }
 
-  fetchKeys(function (err, keys) {
+  fetchDocuments(function (err, docs) {
     if (err) {
       opbeat.captureError(err)
       res.statusCode = 500
@@ -24,7 +27,7 @@ var server = http.createServer(function (req, res) {
       return
     }
 
-    var html = tmpl({ jobs: keys })
+    var html = tmpl({ docs: docs })
     res.writeHead(200, {
       'Content-Length': Buffer.byteLength(html),
       'Content-Type': 'text/html'
@@ -37,11 +40,36 @@ server.listen(process.env.PORT, function () {
   console.log('Server listening on port', server.address().port)
 })
 
+function fetchDocuments (cb) {
+  fetchKeys(function (err, keys) {
+    if (err) return cb(err)
+
+    var next = afterAll(function (err) {
+      if (err) return cb(err)
+      cb(null, docs)
+    })
+
+    var docs = keys.map(function (key) {
+      var doc = { key: key, name: names[key] }
+
+      if (!doc.name) {
+        console.log('Caching document name for %s...', key)
+        var done = next()
+        var params = { Bucket: BUCKET, Key: key }
+        s3.headObject(params, function (err, data) {
+          if (err) return done(err)
+          doc.name = names[key] = data.Metadata.name || key
+          done()
+        })
+      }
+
+      return doc
+    })
+  })
+}
+
 function fetchKeys (cb) {
-  var params = {
-    Bucket: 'watson-printbin'
-  }
-  s3.listObjects(params, function (err, data) {
+  s3.listObjects({ Bucket: BUCKET }, function (err, data) {
     if (err) return cb(err)
     var keys = data.Contents
       .sort(function (a, b) {
